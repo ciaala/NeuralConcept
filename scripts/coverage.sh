ebeebd3c348f1fb07ffc78dd5cb3ec0eb9f6c54b
Francesco Fiduccia <francesco.fiduccia@gmail.com>
2024-03-07 22:34:17 +0100

    better handling of coverage not test files

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-coverage run -m pytest
+# generate coverage report for all the files in the package app, even those not tested
+coverage run --branch --source=app -m pytest
+# generate an html report
 coverage html
+
+OS="$(uname)"
+
+# Open the HTML coverage report with the default browser
+if [ "$OS" = "Darwin" ]; then
+    # macOS
+    open ./htmlcov/index.html
+elif [ "$OS" = "Linux" ]; then
+    # Linux
+    xdg-open ./htmlcov/index.html &
+else
+    echo "Unsupported OS: $OS"
+fi
